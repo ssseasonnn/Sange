9959b661741a60ecde70f723fb84517ad22cb12c
ssseasonnn <ssseasonnn@gmail.com>
2019-07-25 14:45:15 +0800

    fix gc problem

--- app/src/main/java/zlc/season/sangedemo/MainActivity.kt
+++ app/src/main/java/zlc/season/sangedemo/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import zlc.season.sangedemo.demo.DemoActivity
-import zlc.season.sangedemo.demo.normal.NormalActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,12 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btn_normal.setOnClickListener {
-            startActivity(Intent(this, NormalActivity::class.java))
-        }
-
-        btn_multi.setOnClickListener {
             startActivity(Intent(this, DemoActivity::class.java))
         }
-
     }
 }
--- sange/src/main/java/zlc/season/sange/SangeAdapter.kt
+++ sange/src/main/java/zlc/season/sange/SangeAdapter.kt
@@ -3,6 +3,7 @@ package zlc.season.sange
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.RecyclerView.Adapter
 import android.support.v7.widget.RecyclerView.ViewHolder
+import android.view.View
 
 abstract class SangeAdapter<T : Any, VH : ViewHolder>(
     protected open val dataSource: DataSource<T>
@@ -10,6 +11,16 @@ abstract class SangeAdapter<T : Any, VH : ViewHolder>(
 
     override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
         super.onAttachedToRecyclerView(recyclerView)
+        recyclerView.addOnAttachStateChangeListener(
+            object : View.OnAttachStateChangeListener {
+                override fun onViewDetachedFromWindow(v: View?) {
+                    dataSource.setAdapter(null)
+                    recyclerView.removeOnAttachStateChangeListener(this)
+                }
+
+                override fun onViewAttachedToWindow(v: View?) {
+                }
+            })
         dataSource.setAdapter(this)
     }
 
